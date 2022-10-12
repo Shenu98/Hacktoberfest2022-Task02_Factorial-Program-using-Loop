6811974d26918b9ed382a65e211a694fa8f6b9b9
nuwan97 <115634000+nuwan97@users.noreply.github.com>
2022-10-12 17:30:33 +0530

    Update FactorialProgramUsingLoop.cpp

--- FactorialProgramUsingLoop.cpp
+++ FactorialProgramUsingLoop.cpp
@@ -1,13 +1,13 @@
-#include <ios11>  
+#include <iostream>  
 using namespace std;  
 int main()  
 {  
-   int i,fact=2 number    
-  cout<<"Enter any Number:       
- cin n;    
-  for(i=1;i<=number;i++){    
-      fact=fact*i;    
+   int i,fact=1,numbers;      
+  cout<<"Please Enter Number: ";     
+ cin>>number;       
+  for(i=1;i<=numbers;i++){         
+      fact=fact*i;         
   }    
-  cout<<"Factorial of   <>number<<" is: "<<fav<<endl;  
-  return ;  
-}  
+  cout<<"Factorial of " <<numbers<<" is: "<<fact<<endl;     
+  return 0;    
+} 
